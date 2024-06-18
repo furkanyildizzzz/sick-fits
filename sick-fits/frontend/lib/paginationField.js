bbbb9b1718cb6ecaf9d03755ac87509772cc2d76
furkotikoo <furkanyildiz47@gmail.com>
2024-06-18 19:12:06 +0300

    prevent the page number to exceed the total pages and being zero or below

--- sick-fits/frontend/lib/paginationField.js
+++ sick-fits/frontend/lib/paginationField.js
@@ -13,10 +13,13 @@ const paginationField = () => {
       const page = offset / limit + 1;
       const pages = Math.ceil(count / limit);
 
+      if (page > pages || page <= 0) {
+        console.warn(`Requested page ${page} exceeds total pages ${pages}`);
+        return [];
+      }
       const items = existing.slice(offset, offset + limit).filter((x) => x);
       //if there are items AND there aren't enough items to satisfy how many were requessted AND we are on the last page THEN return items
       if (items.length && items.length !== limit && page === pages) return items;
-
       //check if we have existing items
       if (items.length !== limit) return false;
 
@@ -29,6 +32,7 @@ const paginationField = () => {
     // the existing list items.
     merge: (existing, incoming, { args: { skip: offset = 0 } }) => {
       const merged = existing ? existing.slice(0) : [];
+      console.log({ existing, incoming, offset });
 
       for (let i = 0; i < incoming.length; ++i) {
         merged[offset + i] = incoming[i];
